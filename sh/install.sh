1e1e71a187597654792f64237adaa3f45507018e
Tom Power <tpower21@gmail.com>
2024-04-18 18:55:11 +0100

    install.sh fix

--- sh/install.sh
+++ sh/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sh/build.sh &&
 if [[ -f sh/.install.env ]]; then
-  source .install.env
+  source sh/.install.env
   chmod +x release/sshRun && cp -p release/sshRun $binPath
   chmod +x go/build/_sshRun && cp -p go/build/_sshRun $binPath
 fi
