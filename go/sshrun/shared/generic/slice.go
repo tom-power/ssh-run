4d51f85c3baf63b74c3fe55cd689de8abb879c2e
Tom Power <tpower21@gmail.com>
2024-05-17 07:25:22 +0100

    tidy

--- go/sshrun/shared/generic/slice.go
+++ go/sshrun/shared/generic/slice.go
@@ -4,28 +4,28 @@ import (
 	"errors"
 )
 
-func Map[V any, W any](slice []V, fn func(V) W) []W {
-	tmp := make([]W, len(slice))
-	for i, s := range slice {
-		tmp[i] = fn(s)
+func Map[V any, W any](values []V, fn func(V) W) []W {
+	tmp := make([]W, len(values))
+	for i, v := range values {
+		tmp[i] = fn(v)
 	}
 	return tmp
 }
 
-func Filter[V any](slice []V, predicate func(V) bool) []V {
+func Filter[V any](values []V, predicate func(V) bool) []V {
 	var filtered []V
-	for _, s := range slice {
-		if predicate(s) {
-			filtered = append(filtered, s)
+	for _, v := range values {
+		if predicate(v) {
+			filtered = append(filtered, v)
 		}
 	}
 	return filtered
 }
 
 func Single[V any](values []V, predicate func(V) bool) (*V, error) {
-	for _, value := range values {
-		if predicate(value) {
-			return &value, nil
+	for _, v := range values {
+		if predicate(v) {
+			return &v, nil
 		}
 	}
 	return nil, errors.New("couldn't find value")
@@ -39,8 +39,8 @@ func LastOr[V any](values []V, defaultV V) V {
 }
 
 func Any[V comparable](values []V, value V) bool {
-	for _, item := range values {
-		if item == value {
+	for _, v := range values {
+		if v == value {
 			return true
 		}
 	}
@@ -48,8 +48,8 @@ func Any[V comparable](values []V, value V) bool {
 }
 
 func All[V comparable](values []V, value V) bool {
-	for _, item := range values {
-		if item != value {
+	for _, v := range values {
+		if v != value {
 			return false
 		}
 	}
@@ -57,9 +57,9 @@ func All[V comparable](values []V, value V) bool {
 }
 
 func Intersect[V comparable](values []V, otherValues []V) bool {
-	for _, item := range values {
+	for _, v := range values {
 		for _, otherItem := range otherValues {
-			if item == otherItem {
+			if v == otherItem {
 				return true
 			}
 		}
@@ -67,9 +67,9 @@ func Intersect[V comparable](values []V, otherValues []V) bool {
 	return false
 }
 
-func GetOr[V any](slice []V, at int, defaultValue V) V {
-	if len(slice) > at {
-		return slice[at]
+func GetOr[V any](values []V, at int, defaultValue V) V {
+	if len(values) > at {
+		return values[at]
 	}
 	return defaultValue
 }
