11d47b0656dfea66f9e8276199de1d4c187c29f5
Tom Power <tpower21@gmail.com>
2024-12-14 18:37:29 +0000

    fix bad syntax in sh/install.sh

--- sh/install.sh
+++ sh/install.sh
@@ -2,12 +2,12 @@
 sh/build.sh &&
 if [[ -f sh/.install.env ]]; then
   source sh/.install.env
-  if [[ -v binPath ]]; then
+  if [ -n "${binPath+set}" ]; then
     echo "copying scripts"
     cp -p release/sshRun $binPath/sshRun && chmod +x $binPath/sshRun &&
     cp -p go/build/_sshRun $binPath/_sshRun && chmod +x $binPath/_sshRun
   fi
-  if [[ -v completionsPath ]]; then
+  if [ -n "${completionsPath+set}" ]; then
     echo "copying completions"
     cp -p release/completions/_sshRun $completionsPath/_sshRun
   fi
